a00c4a9644a69fc48b82f75cb2a4163f532cb90c
anandagarwal <anandagarwal@djalexander.in>
2017-12-01 11:35:42 +0530

    Completion of tddworkshop all assignment

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -2,7 +2,63 @@
 namespace phpreboot\tddworkshop;
 
 class Calculator{
-    public function add(){
-        return 0;
+
+    public function add($numbers = '')
+    {
+        if (empty($numbers)) {
+            return 0;
+        }
+        if (!is_string($numbers) ) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+
+        $numbers = str_replace("\\\\","", $numbers);
+        $negativeNumbers=array();
+        $numbersArray = $this->multiexplode(array("\\n",",",";"), $numbers);
+        $numbersArray = array_filter($numbersArray, function($number){
+            return $number < 1000;
+        });
+        $negativeNumbers = array_filter($numbersArray, function($number){
+                return $number < 0;
+        });
+
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        } elseif (!empty($negativeNumbers)){
+            throw new \InvalidArgumentException('Negative numbers ('.implode(',',$negativeNumbers).') not allowed');
+        }
+        return array_sum($numbersArray);
+    }
+
+    function multiexplode ($delimiters,$string) {
+
+        $ready = str_replace($delimiters, $delimiters[0], $string);
+        $launch = explode($delimiters[0], $ready);
+        return  $launch;
+    }
+
+    public function multiply($numbers = '')
+    {
+        if (empty($numbers)) {
+            return 0;
+        }
+        if (!is_string($numbers) ) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+        $numbers = str_replace("\\\\","", $numbers);
+        $negativeNumbers=array();
+        $numbersArray = $this->multiexplode(array("\\n",",",";"), $numbers);
+        $numbersArray = array_filter($numbersArray, function($number){
+            return $number < 1000;
+        });
+        $negativeNumbers = array_filter($numbersArray, function($number){
+            return $number < 0;
+        });
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        } elseif (!empty($negativeNumbers)){
+            throw new \InvalidArgumentException('Negative numbers ('.implode(',',$negativeNumbers).') not allowed');
+        }
+        return array_product($numbersArray);
     }
 }
--- tests/phpreboot/tddworkshop/CalculatorTest.php
+++ tests/phpreboot/tddworkshop/CalculatorTest.php
@@ -24,4 +24,132 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
 
         $this->assertInternalType('integer', $result, 'Result of `add` is not an integer.');
     }
+
+    public function testAddWithoutParameterReturnsZero()
+    {
+        $result = $this->calculator->add();
+        $this->assertSame(0, $result, 'Empty string on add do not return 0');
+    }
+
+    public function testAddWithSingleNumberReturnsSameNumber()
+    {
+        $result = $this->calculator->add('3');
+        $this->assertSame(3, $result, 'Add with single number do not returns same number');
+    }
+
+    public function testAddWithTwoParametersReturnsTheirSum()
+    {
+        $result = $this->calculator->add('2,4');
+        $this->assertSame(6, $result, 'Add with two parameter do not returns correct sum');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function  testAddWithNonStringParameterThrowsException()
+    {
+        $this->calculator->add(5, 'Integer parameter do not throw error');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testAddWithNonNumbersThrowException()
+    {
+        $this->calculator->add('1,a', 'Invalid parameter do not throw exception');
+    }
+
+    public function addDataProvider() {
+        return array(
+            array('1,2,3'),
+            array('1,2,3,6'),
+            array('1,2,3,4,10'),
+            array('0,0,0'),
+        );
+    }
+
+    /**
+     * @dataProvider addDataProvider
+     */
+    public function testAddMultipleNumbers($a)
+    {
+        $result = $this->calculator->add($a);
+        $this->assertInternalType('integer', $result, 'Add with multiple parameter does not matches the expected result');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testAddWithNegativeNumbersThrowException()
+    {
+        $this->calculator->add('1,-2', 'Negative numbers not allowed');
+    }
+
+    /*Test for multiply*/
+    public function testMultiplyReturnsAnInteger()
+    {
+        $result = $this->calculator->multiply();
+
+        $this->assertInternalType('integer', $result, 'Result of `product` is not an integer.');
+    }
+
+    public function testMultiplyWithoutParameterReturnsZero()
+    {
+        $result = $this->calculator->multiply();
+        $this->assertSame(0, $result, 'Empty string on product do not return 0');
+    }
+
+    public function testMultiplyWithSingleNumberReturnsSameNumber()
+    {
+        $result = $this->calculator->multiply('3');
+        $this->assertSame(3, $result, 'Product with single number do not returns same number');
+    }
+
+    public function testMultiplyWithTwoParametersReturnsTheirProduct()
+    {
+        $result = $this->calculator->multiply('2,4');
+        $this->assertSame(8, $result, 'Product with two parameter do not returns correct product');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function  testMultiplyWithNonStringParameterThrowsException()
+    {
+        $this->calculator->multiply(5, 'Integer parameter do not throw error');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testMultiplyWithNonNumbersThrowException()
+    {
+        $this->calculator->multiply('1,a', 'Invalid parameter do not throw exception');
+    }
+
+    public function multiplyDataProvider() {
+        return array(
+            array('1,2,3'),
+            array('1,2,3,6'),
+            array('1,2,3,4,10'),
+            array('0,0,0'),
+        );
+    }
+
+    /**
+     * @dataProvider multiplyDataProvider
+     */
+    public function testMultiplyMultipleNumbers($a)
+    {
+        $result = $this->calculator->multiply($a);
+        $this->assertInternalType('integer', $result, 'Product with multiple parameter does not matches the expected result');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testMultiplyWithNegativeNumbersThrowException()
+    {
+        $this->calculator->multiply('1,-2', 'Negative numbers not allowed');
+    }
 }
